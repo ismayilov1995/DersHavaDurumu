345824d9908c44838b1805f46b805a9ab3009acc
ismayilov1995 <ismayilov1995@users.noreply.github.com>
2018-03-03 17:20:23 +0400

    GPS icazeleri almaq ve optimizasiya

--- app/src/main/java/com/example/ismayil/DersHavaDurumu/MainActivity.kt
+++ app/src/main/java/com/example/ismayil/DersHavaDurumu/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.ismayil.DersHavaDurumu
 
+import android.content.pm.PackageManager
 import android.graphics.PorterDuff
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.AdapterView
@@ -12,27 +15,32 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.Response.Listener
 import com.android.volley.toolbox.JsonObjectRequest
+import im.delight.android.location.SimpleLocation
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
-
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
 
     var tvSpinner:TextView? = null
-
     var capitalArray = ArrayList<String>(5)
+    private var location:SimpleLocation? = null
+    var longitude:String? = null
+    var latitude:String? = null
+    var url:String?=null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        capitalToSpinner()
+        weatherForCities("Baku")
 
-        paytaxtlar()
-        havaDurumu("Ankara")
+
 
 
     }
@@ -41,31 +49,74 @@ class MainActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
     }
 
     override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-        val selectedCity = p0?.getItemAtPosition(p2).toString()
         tvSpinner = p1 as TextView
-        havaDurumu(selectedCity)
 
+        if (p2 == 0){
+            location = SimpleLocation(this)
+            if (!location!!.hasLocationEnabled()){
+                spinner.setSelection(245)
+                Toast.makeText(this,"GPS -i Aktiv Et",Toast.LENGTH_SHORT).show()
+                SimpleLocation.openSettings(this)
+            }else{
+                if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),1)
+                }else{
+                    location = SimpleLocation(this)
+                    latitude = String.format("%.6f",location?.latitude)
+                    longitude = String.format("%.6f",location?.longitude)
+                    weatherForLocation(latitude,longitude)
+                }
+            }
+        }else{
+            val selectedCity = p0?.getItemAtPosition(p2).toString()
+            weatherForCities(selectedCity)
+        }
     }
 
-    fun paytaxtlar(){
-        val urlCapital = "http://country.io/capital.json"
-        val paytaxtObje = JsonObjectRequest(Request.Method.GET, urlCapital, null, Listener<JSONObject?> {
-            val allCitiesAd = it?.names()
-            var paytaxt="Ankara"
-            var paytaxtKod = "AA"
+    private fun weatherForLocation(latitude: String?, longitude: String?) {
+        url = "http://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=abbcd2fcfec741ec783669c98b7f39d1&lang=tr&units=metric"
+        valleyListener(url!!)
+    }
 
+    private fun weatherForCities(cityURI: String) {
+        url = "http://api.openweathermap.org/data/2.5/weather?q=$cityURI&appid=abbcd2fcfec741ec783669c98b7f39d1&lang=tr&units=metric"
+        valleyListener(url!!)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == 1){
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                location = SimpleLocation(this)
+                latitude = String.format("%.6f",location?.latitude)
+                longitude = String.format("%.6f",location?.longitude)
+                weatherForLocation(latitude,longitude)
+            }else{
+                spinner.setSelection(245)
+                Toast.makeText(this,"Nolub aaa Icaze Verde...",Toast.LENGTH_SHORT).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun capitalToSpinner(){
+        val urlCapital = "http://country.io/capital.json"
+        val paytaxtObje = JsonObjectRequest(Request.Method.GET, urlCapital, null, Listener<JSONObject?> { it ->
+            val allCitiesAd = it?.names()
+            var paytaxt="Baku"
+            var paytaxtKod = "AZ"
+
+                capitalArray.add("Indiki Yer")
             for (i in 0 until allCitiesAd!!.length()){
                 paytaxtKod = allCitiesAd[i].toString()
                 paytaxt = it.get(paytaxtKod).toString()
                 capitalArray.add(paytaxt)
             }
-
             val adapter = ArrayAdapter(this,R.layout.spinner_item_style_custom,capitalArray)
-
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinner.background.setColorFilter(resources.getColor(R.color.colorAccent), PorterDuff.Mode.SRC_ATOP)
             spinner.adapter = adapter
             spinner.onItemSelectedListener = this
+            spinner.setSelection(245)
 
         }, Response.ErrorListener {
             Toast.makeText(this@MainActivity, "Siktirifsen", Toast.LENGTH_LONG).show()
@@ -73,23 +124,23 @@ class MainActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
         MySingleton.getInstance(this)?.addToRequestQueue(paytaxtObje)
     }
 
-
-    fun havaDurumu(cityURI: String) {
-        val url = "http://api.openweathermap.org/data/2.5/weather?q=$cityURI&appid=abbcd2fcfec741ec783669c98b7f39d1&lang=tr&units=metric"
+    private fun valleyListener(url:String){
+        var cityName:String?="Yuklenir"
         val havaDurumObje = JsonObjectRequest(Request.Method.GET, url, null, Listener<JSONObject?> {
-
-            tvDate.text = getDate()
 
             val main = it?.getJSONObject("main")
             val tempratur = main?.getInt("temp")
-            tvTemp.text = tempratur.toString()
-
             val weather = it?.getJSONArray("weather")
             val description = weather?.getJSONObject(0)?.getString("description")
-            tvDescription.text = description
-
             val icon = weather?.getJSONObject(0)?.getString("icon")
             val imageFilesName = resources.getIdentifier("icon_" + icon?.removeLastChar(), "drawable", packageName)
+            cityName = it?.getString("name")
+
+            tvSpinner?.text = cityName
+            tvDate.text = getDate()
+            tvTemp.text = tempratur.toString()
+            tvDescription.text = description
+
             if (icon?.last() == 'd') {
                 spinner.background.setColorFilter(resources.getColor(R.color.black), PorterDuff.Mode.SRC_ATOP)
                 rootLayout.background = getDrawable(R.drawable.bg)
@@ -107,24 +158,23 @@ class MainActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
                 tvDegree.setTextColor(resources.getColor(R.color.white))
                 tvSpinner?.setTextColor(resources.getColor(R.color.white))
             }
-
             ivSymbol.setImageResource(imageFilesName)
-
         }, Response.ErrorListener {
+            spinner.setSelection(245)
             Toast.makeText(this,"Secilen Sheher Barede Melumat Tapilmadi",Toast.LENGTH_LONG).show()
         })
         MySingleton.getInstance(this)?.addToRequestQueue(havaDurumObje)
     }
 
-    fun getDate(): String {
+    private fun getDate(): String {
         val calendar = Calendar.getInstance().time
         val formatting = SimpleDateFormat("EEEE, MMMM yyyy", Locale("az"))
         val date = formatting.format(calendar)
 
         return date
     }
-
 }
+
 private fun String.removeLastChar(): String {
     return this.substring(0,this.length-1)
 }
